76bd56f56493c3819b0bbe05d8e5a39d1fd7b413
OddReen <leonardovneves2002@gmail.com>
2023-04-18 12:08:34 +0100

    MonoScript Change

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Animator))]
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject glideGameObject;// glide test
@@ -24,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float turboTargetSpeed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float maxTurboSpeed;
+    [SerializeField] private float goingDownGroundSpeedTargetMultiplier;
     [SerializeField] private float goingDownGroundSpeedMultiplier;
 
     [Header("Transition")]
@@ -71,8 +73,24 @@ public class PlayerMovement : MonoBehaviour
     private Animator _animator;
     private Rigidbody _rigidbody;
 
-    private Vector3 previousNormals;
-    private Quaternion fromTo;
+    //OnSlopeBehaviour
+    [SerializeField] private float smoothTimeTargetRotation;
+
+    [SerializeField] private float playerForceToGround;
+
+    [Header("Activables")]
+    [SerializeField] private bool onSlopePosition;
+    [SerializeField] private bool onSlopeRotation;
+
+    [Header("RayCastHits")]
+    [SerializeField] private float rayCastLenght = 1f;
+
+    [SerializeField] private float heightToAirborne = 2f;
+
+    private Vector3 averageNormal;
+    private Quaternion targetRotation;
+    private int baseRayCastCount = 0;
+    private bool baseRayCastDetection = false;
 
     public enum State
     {
@@ -82,6 +100,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
+        GetComponent<Renderer>().material.SetColor(
+            "_Color", PlayerMovement._state == PlayerMovement.State.Grounded ? Color.black : Color.blue
+        );
         _stateSF = _state;
     }
     private void Awake()
@@ -100,6 +121,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (onSlopePosition && PlayerMovement._state == PlayerMovement.State.Grounded) OnSlopePosition();
+        if (onSlopeRotation && PlayerMovement._state == PlayerMovement.State.Grounded) OnSlopeRotation();
         if (detectIsGrounded)
         {
             Grounded();
@@ -107,14 +130,25 @@ public class PlayerMovement : MonoBehaviour
         if (_state == State.Grounded)
         {
             _rigidbody.useGravity = false;
-            ApplyGroundTurbo();
+
+            if (!Physics.Raycast(transform.position, -transform.up, heightToAirborne))
+            {
+                detectIsGrounded = false;
+                Invoke("OffGround", .5f);
+                _state = State.Airborne;
+                return;
+            }
             //!Inputing
             if (_input.magnitude == 0 && !isTurboing)
             {
                 currentSpeed = Mathf.SmoothDamp(currentSpeed, 0f, ref _deccelarationSmoothVelocity, deccelaration);
                 _rigidbody.velocity = new Vector3(transform.forward.x * currentSpeed * Time.deltaTime, transform.forward.y * currentSpeed * Time.deltaTime, transform.forward.z * currentSpeed * Time.deltaTime);
+                fov = Mathf.SmoothDamp(fov, fovNormal, ref fovCurrentVelocity, fovSmoothTime);
+                _cinemachineFreeLook.m_Lens.FieldOfView = fov;
                 return;
             }
+
+            ApplyGroundTurbo();
             ApplyGroundRotation();
             ApplyGroundMovement();
             return;
@@ -123,8 +157,6 @@ public class PlayerMovement : MonoBehaviour
         {
             _rigidbody.useGravity = true;
             ApplyAirbornRotation();
-            ApplyAirbornMovement();
-            ApplyAirbornTurbo();
         }
         if (_state == State.OpenWings)
         {
@@ -169,17 +201,16 @@ public class PlayerMovement : MonoBehaviour
     }
     private void ApplyGroundMovement()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit))
+        if (transform.rotation.x < -15f)
         {
-            fromTo = Quaternion.FromToRotation(previousNormals, hit.normal);
-            previousNormals = hit.normal;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, groundTargetSpeed, ref _accelerationSmoothVelocity, acceleration);
+            _rigidbody.velocity = new Vector3(transform.forward.x * currentSpeed * 2f * Time.deltaTime, transform.forward.y * currentSpeed * 2f * Time.deltaTime, transform.forward.z * currentSpeed * 2f * Time.deltaTime);
+            return;
         }
         if (_input.magnitude != 0 && !isTurboing)
         {
             currentSpeed = Mathf.SmoothDamp(currentSpeed, groundTargetSpeed, ref _accelerationSmoothVelocity, acceleration);
             _rigidbody.velocity = new Vector3(transform.forward.x * currentSpeed * Time.deltaTime, transform.forward.y * currentSpeed * Time.deltaTime, transform.forward.z * currentSpeed * Time.deltaTime);
-            _rigidbody.velocity = fromTo * _rigidbody.velocity;
         }
     }
     public void ApplyGroundTurbo()
@@ -192,12 +223,6 @@ public class PlayerMovement : MonoBehaviour
 
             currentSpeed = Mathf.SmoothDamp(currentSpeed, turboTargetSpeed, ref _turboAccelerationSmoothVelocity, turboAcceleration);
             _rigidbody.velocity = new Vector3(transform.forward.x * currentSpeed * Time.deltaTime, transform.forward.y * currentSpeed * Time.deltaTime, transform.forward.z * currentSpeed * Time.deltaTime);
-            _rigidbody.velocity = fromTo * _rigidbody.velocity;
-        }
-        else
-        {
-            fov = Mathf.SmoothDamp(fov, fovNormal, ref fovCurrentVelocity, fovSmoothTime);
-            _cinemachineFreeLook.m_Lens.FieldOfView = fov;
         }
     }
 
@@ -210,36 +235,41 @@ public class PlayerMovement : MonoBehaviour
         if ((_state == State.Airborne || _state == State.OpenWings) && _input.magnitude == 0f)
         {
             //Roll
-            float angleRoll = Mathf.SmoothDampAngle(transform.eulerAngles.z, 0f, ref _turnSmoothVelocity, rollTurnSmoothTime);
+            float angleRoll = Mathf.LerpAngle(_rigidbody.rotation.eulerAngles.z, 0f, rollTurnSmoothTime * Time.deltaTime);
             _rigidbody.MoveRotation(Quaternion.Euler(_rigidbody.rotation.eulerAngles.x, _rigidbody.rotation.eulerAngles.y, angleRoll));
         }
 
+        //AD
         //Yaw
         if (_direction.x != 0f)
         {
-            float targetAngleYaw = Mathf.Atan2(cameraRelativeMovement.x, cameraRelativeMovement.z) * Mathf.Rad2Deg;
-            float angleYaw = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngleYaw, ref _turnSmoothVelocity, yawTurnSmoothTime);
 
-            float targetAngleRoll = Mathf.Atan2(cameraRelativeMovement.x, cameraRelativeMovement.y) * Mathf.Rad2Deg;
-            float angleRoll = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngleRoll, ref _turnSmoothVelocity, rollTurnSmoothTime);
+            float targetAngleRoll = Mathf.Atan2(-_direction.x, _direction.y) * Mathf.Rad2Deg;
+            targetAngleRoll *= .5f;
+            float angleRoll = Mathf.LerpAngle(_rigidbody.rotation.eulerAngles.z, targetAngleRoll, rollTurnSmoothTime * Time.deltaTime);
+
+            float targetAngleYaw = Mathf.Atan2(cameraRelativeMovement.x, cameraRelativeMovement.z) * Mathf.Rad2Deg;
+            float angleYaw = Mathf.LerpAngle(_rigidbody.rotation.eulerAngles.y, targetAngleYaw, yawTurnSmoothTime * Time.deltaTime);
 
             _rigidbody.MoveRotation(Quaternion.Euler(_rigidbody.rotation.eulerAngles.x, angleYaw, angleRoll));
         }
 
+        //WS
         //Pitch
         if (_direction.z != 0f)
         {
-            float targetAnglePitch = Mathf.Atan2(-cameraRelativeMovement.z, -cameraRelativeMovement.y) * Mathf.Rad2Deg;
+            float targetAnglePitch = Mathf.Atan2(_direction.z, _direction.y) * Mathf.Rad2Deg;
+
             if (targetAnglePitch > 0)
             {
-                float anglePitch = Mathf.SmoothDampAngle(transform.eulerAngles.x, targetAnglePitch * 1f, ref _turnSmoothVelocity, pitchTurnSmoothTime);
-                Debug.Log(transform.eulerAngles.x + "" + targetAnglePitch);
+                float anglePitch = Mathf.LerpAngle(_rigidbody.rotation.eulerAngles.x, targetAnglePitch * 1f, pitchTurnSmoothTime * Time.deltaTime);
+
                 _rigidbody.MoveRotation(Quaternion.Euler(anglePitch, _rigidbody.rotation.eulerAngles.y, _rigidbody.rotation.eulerAngles.z));
             }
             if (targetAnglePitch < 0)
             {
-                float anglePitch = Mathf.SmoothDampAngle(transform.eulerAngles.x, targetAnglePitch * 0.5f, ref _turnSmoothVelocity, pitchTurnSmoothTime);
-                Debug.Log(transform.eulerAngles.x + "" + targetAnglePitch);
+                float anglePitch = Mathf.LerpAngle(_rigidbody.rotation.eulerAngles.x, targetAnglePitch * .5f, pitchTurnSmoothTime * Time.deltaTime);
+
                 _rigidbody.MoveRotation(Quaternion.Euler(anglePitch, _rigidbody.rotation.eulerAngles.y, _rigidbody.rotation.eulerAngles.z));
             }
         }
@@ -277,7 +307,6 @@ public class PlayerMovement : MonoBehaviour
         if (_state == State.Airborne || _state == State.OpenWings)
         {
             RaycastHit hit;
-            Debug.DrawRay(transform.position, Vector3.down * 10f);
             if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f, _layerMask))
             {
                 _state = State.Grounded;
@@ -296,6 +325,7 @@ public class PlayerMovement : MonoBehaviour
                 detectIsGrounded = false;
                 Invoke("OffGround", .5f);
                 _state = State.Airborne;
+
                 Vector3 ponceDir = (transform.forward + hit.normal) / 2;
                 _rigidbody.AddForce(ponceDir * pounceForce);
             }
@@ -320,5 +350,83 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed) { isTurboing = true; }
         if (context.canceled) { isTurboing = false; }
+    }
+
+    //OnSlopeBehaviour
+    private void OnSlopePosition()
+    {
+        if (!Physics.Raycast(transform.position, -transform.up))
+        {
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, new Vector3(_rigidbody.rotation.eulerAngles.x, _rigidbody.rotation.eulerAngles.y, 0f));
+            //targetRotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, Time.deltaTime * 100);
+            _rigidbody.MoveRotation(targetRotation);
+            return;
+        }
+        if (!Physics.Raycast(transform.position, -transform.up, .6f))
+        {
+            _rigidbody.AddForce(-transform.up * playerForceToGround);
+        }
+        else
+        {
+            targetRotation = Quaternion.FromToRotation(transform.up, new Vector3(_rigidbody.rotation.x, _rigidbody.rotation.y, 0f));
+            targetRotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, Time.deltaTime * 10);
+            _rigidbody.MoveRotation(targetRotation);
+        }
+    }
+    private void OnSlopeRotation()
+    {
+        baseRayCastDetection = false;
+
+        baseRayCastCount = 0;
+
+        BaseRayCasts(transform.position, transform.forward, Vector3.zero, Vector3.zero); // Fowards
+        BaseRayCasts(transform.position, transform.forward, Vector3.zero, Vector3.zero); // Fowards
+        BaseRayCasts(transform.position, transform.forward, Vector3.zero, Vector3.zero); // Fowards
+
+        BaseRayCasts(transform.position, -transform.up, Vector3.zero, Vector3.zero); //Downwards
+        BaseRayCasts(transform.position, -transform.up, Vector3.forward, Vector3.zero); // Fowards
+        BaseRayCasts(transform.position, -transform.up, Vector3.right, Vector3.zero); // Right
+        BaseRayCasts(transform.position, -transform.up, Vector3.left, Vector3.zero); // Left
+
+        BaseRayCasts(transform.position, -transform.up, Vector3.forward, Vector3.right);
+        BaseRayCasts(transform.position, -transform.up, Vector3.forward, Vector3.left);
+        BaseRayCasts(transform.position, -transform.up, Vector3.back, Vector3.right);
+        BaseRayCasts(transform.position, -transform.up, Vector3.back, Vector3.left);
+
+        if (baseRayCastDetection)
+        {
+            averageNormal /= baseRayCastCount;
+
+            targetRotation = Quaternion.FromToRotation(transform.up, averageNormal) * transform.rotation;
+            targetRotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, Time.deltaTime * smoothTimeTargetRotation);
+            _rigidbody.MoveRotation(targetRotation);
+        }
+    }
+    private void BaseRayCasts(Vector3 origin, Vector3 heightDir, Vector3 firstDir, Vector3 secondDir)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(origin, heightDir + (firstDir + secondDir).normalized, out hit, rayCastLenght, _layerMask))
+        {
+            baseRayCastDetection = true;
+            baseRayCastCount++;
+            averageNormal += hit.normal;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, (transform.forward + (Vector3.zero + Vector3.zero).normalized) * rayCastLenght);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.zero + Vector3.zero).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.forward + Vector3.zero).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.right + Vector3.zero).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.left + Vector3.zero).normalized) * rayCastLenght);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.forward + Vector3.right).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.forward + Vector3.left).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.back + Vector3.right).normalized) * rayCastLenght);
+        Gizmos.DrawRay(transform.position, (-transform.up + (Vector3.back + Vector3.left).normalized) * rayCastLenght);
     }
 }
