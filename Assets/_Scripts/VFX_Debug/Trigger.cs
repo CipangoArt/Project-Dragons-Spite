0cae1692b2bed808341cd66d5bd7a001d5c20ee6
CipangoArt <114663221+CipangoArt@users.noreply.github.com>
2023-06-01 00:13:17 +0100

    FireBall Explosion Fix

--- Assets/_Scripts/VFX_Debug/Trigger.cs
+++ Assets/_Scripts/VFX_Debug/Trigger.cs
@@ -6,11 +6,18 @@ public class Trigger : MonoBehaviour
 {
     [Header("Press P to trigger VFX prefab in the sphere next to the well in the map")]
     [SerializeField] Transform spawnPos;
+    [SerializeField] GameObject VFX_to_Debug;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Instantiate(VFX_to_Debug, transform.position, VFX_to_Debug.transform.rotation);
+        }
+    }
 }
