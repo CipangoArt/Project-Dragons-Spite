72843503634d822a6f2d4f8670f57c705719e3c0
CipangoArt <114663221+CipangoArt@users.noreply.github.com>
2023-07-12 17:10:56 +0100

    Lighting glitch fix

---
+++ Assets/Scripts/VFX/FractureObject/FractureObject.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class FractureObject : MonoBehaviour
+{
+    [SerializeField] private string[] sounds;
+    private GameObject originalObject;
+    [SerializeField] private GameObject fracturedObject;
+    [SerializeField] private float explosionMinForce = 5;
+    [SerializeField] private float explosionMaxForce = 100;
+    [SerializeField] private float explosionForceRadius = 10;
+    public bool isThrown = false;
+    [SerializeField] bool isbreakable = true;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        originalObject = gameObject;
+    }
+    public void Fracture()
+    {
+
+        if (sounds.Length != 0)
+        {
+            AudioManager.instance.PlaySound(sounds[Random.Range(0, sounds.Length - 1)]);
+        }
+        if (isbreakable)
+        {
+            if (originalObject != null)
+            {
+                originalObject.SetActive(false);
+                if (fracturedObject != null)
+                {
+                    GameObject fractObj = Instantiate(fracturedObject, originalObject.transform.position, Quaternion.identity) as GameObject;
+                    foreach (Transform t in fractObj.transform)
+                    {
+                        rb = t.GetComponent<Rigidbody>();
+                        if (rb != null)
+                            rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionForceRadius);
+                        Destroy(originalObject);
+                        Invoke("DisableCollisions", 5);
+                    }
+                }
+            }
+        }
+
+    }
+    private void DisableCollisions()
+    {
+        rb.detectCollisions = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isThrown && !collision.gameObject.CompareTag("Player"))
+        {
+            isThrown = false;
+            Fracture();
+            if (collision.gameObject.GetComponent<FractureObject>() != null)
+            {
+                collision.gameObject.GetComponent<FractureObject>().Fracture();
+            }
+            
+        }
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
